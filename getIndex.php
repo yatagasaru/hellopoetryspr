7f203cf8aaf8eca44b4d9ec1f6407cd84b5ce2dd
ga <rpkaad@gmail.com>
2018-07-07 15:24:37 +0700

    add resource selection to ensure client get the latest poems

--- getIndex.php
+++ getIndex.php
@@ -1,10 +1,11 @@
 <?php
-    //gladysbot google image parser template//
-    $url =  "https://hellopoetry.com/ga/poems/";
-    //include_once "selectSource.php";
-    //$url = "https://hellopoetry.com/ga/";
-    define('FAKE_USER_AGENT', "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
+    //++gladysbot google image parser template++//
     
+    //$url =  "https://hellopoetry.com/ga/poems/";
+    //$url = "https://hellopoetry.com/ga/";
+    include_once "selectSource.php";
+    define('FAKE_USER_AGENT', "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
+
     function getPage($url){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -16,15 +17,41 @@
         return $data;
     }
     
-     $rawpage = getPage($url);
+    $poemurlTemp;
+    $poemurl = array();
+    $source = selectSource();
+    $url = $source["url"];
 
-     preg_match_all('|<a href="/(.*?)" class="poem-title nocolor">|', $rawpage, $poemurl);
-     foreach($poemurl[1] as &$val){
-        $val = "https://hellopoetry.com/".$val;
-     }
+    function fromPoems($url){
+        global $poemurl, $poemurlTemp;
+        $rawpage = getPage($url);
+        preg_match_all('|<a href="/(.*?)" class="poem-title nocolor">|', $rawpage, $poemurlTemp);
 
-    $json =  $poemurl[1];
-    $json['count'] = sizeof($poemurl[1]);
+        foreach($poemurlTemp[1] as $val){
+            array_push($poemurl, "https://hellopoetry.com/".$val);
+        }
+    }
+    
+    function fromStream($url){
+        global $poemurl, $poemurlTemp;
+        $rawpage = getPage($url);
+        preg_match('|<a href="/(.*?)" class="nocolor">|', $rawpage, $poemurlTemp);
+    
+        foreach((array)$poemurlTemp[1] as $val){
+            array_push($poemurl, "https://hellopoetry.com/".$val);
+        }
+    }
+
+    if($source["count"] === "stream"){
+        fromStream($url);
+        fromPoems("https://hellopoetry.com/ga/poems/");
+    }
+    else{
+        fromPoems($url);
+    }
+
+    $json =  $poemurl;
+    $json['count'] = sizeof($poemurl);
     $json['status'] = "OK";
     header('Content-Type: application/json');
     echo json_encode($json, JSON_PRETTY_PRINT);
